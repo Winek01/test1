e4bfb1cba5863fd6c3d0c9851fa6c64b3d9473e5
Winek01 <137410612+Winek01@users.noreply.github.com>
2023-06-24 11:27:53 +0200

    added C++
    dodano plik źródłowy języka C++

---
+++ main (3).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string filename;
+  char characterToCount;
+  int characterCount = 0;
+
+  cout << "Podaj nazwę pliku: ";
+  cin >> filename;
+  cout << "Podaj znak do sprawdzenia: ";
+  cin >> characterToCount;
+
+  ifstream file;
+  file.open(filename);
+
+  if (!file.is_open()) {
+    cout << "Nie udało się otworzyć pliku." << endl;
+    return 1;
+  }
+
+  char currentCharacter;
+  while (file.get(currentCharacter)) {
+    if (currentCharacter == characterToCount) {
+      characterCount++;
+    }
+  }
+
+  cout << "Liczba znaków '" << characterToCount << "' w pliku '" << filename << "': " << characterCount << endl;
+
+  return 0;
+}
