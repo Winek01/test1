194448f71aa43f7f06476a01be962439940c654d
Winek01 <137410612+Winek01@users.noreply.github.com>
2023-06-24 11:34:25 +0200

    Update main Java
    dodano aktualizację do pliku źródłowego języka Java

--- main Java.java
+++ main Java.java
@@ -2,5 +2,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Aktualizacja");
+		System.out.println("Aktualizacja2");
 	}
 }
